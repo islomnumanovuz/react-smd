3d8961d03494fe64c7b543c5ebce92b14e862c89
islomnumanov <islomnumanov.uz@gmail.com>
2022-10-30 07:57:44 +0300

    feat: Add functionality with simple variable

--- src/components/State/State.jsx
+++ src/components/State/State.jsx
@@ -4,13 +4,25 @@ export class State extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+    console.log("constructor");
   }
   render() {
-    const count = 0;
+    console.log('render');
+    let count = 0;
+    const plus = () => {
+      count += 1
+      console.log(count);
+    }
+    const minus = () => {
+      count -= 1
+      console.log(count);
+    }
     return (
       <div>
         <h1>Hi webbrain academy</h1>
         <h3>Count: {count}</h3>
+        <button onClick={minus}>-</button>
+        <button onClick={plus}>+</button>
       </div>
     )
   }
