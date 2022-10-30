3d2a07fca9b5b6d071f289981f11ea3d29296fda
islomnumanov <islomnumanov.uz@gmail.com>
2022-10-30 08:10:47 +0300

    feat: Add onSelect function

--- src/components/Inputs/Inputs.jsx
+++ src/components/Inputs/Inputs.jsx
@@ -11,10 +11,17 @@ export class Inputs extends Component {
     const onChange = (e) => {
       this.setState({title: e.target.value})
     }
+    const onSelect = (e) => {
+      console.log(e.target.value);
+    }
     return(
       <form>
         <h1>This is {this.state.title}</h1>
         <input onChange={onChange} type="text" placeholder="name" />
+        <select onChange={onSelect} name="select" id="select">
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
       </form>
     )
   }
