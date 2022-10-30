88635f9f76494084be1d014232b3e9cffc0cb399
islomnumanov <islomnumanov.uz@gmail.com>
2022-10-30 13:09:18 +0300

    feat: Add two functions

--- src/components/State/State.jsx
+++ src/components/State/State.jsx
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
 
 export class State extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "Islom",
+      surname: "Kabulov"
+    }
+  }
   render() {
-    const count = 0;
+    const onChangeName = e => {
+      this.setState({
+        name: e.target.value
+      })
+    }
+    const onChangeSurname = e => {
+      this.setState({
+        surname: e.target.value
+      })
+    }
     return (
       <div>
-        <h1>Hi webbrain academy</h1>
-        <h3>Count: {count}</h3>
+        <form action="#">
+          <h2>Name {this.state.name}</h2>
+          <h2>Surname {this.state.surname}</h2>
+          <input onChange={onChangeName} type="text" placeholder="name" />
+          <input onChange={onChangeSurname} type="text" placeholder="surname" />
+        </form>
       </div>
     )
   }
