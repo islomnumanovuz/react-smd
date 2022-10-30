fd4e46f4cba5d914d362e118e799693f6d716b6f
islomnumanov <islomnumanov.uz@gmail.com>
2022-10-30 07:47:56 +0300

    feat: Add initial state

---
+++ src/components/State/State.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+export class State extends Component {
+  render() {
+    const count = 0;
+    return (
+      <div>
+        <h1>Hi webbrain academy</h1>
+        <h3>Count: {count}</h3>
+      </div>
+    )
+  }
+}
