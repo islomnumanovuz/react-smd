e74d27ac957f1ac94430b95e6bf078ac4ca2a8d1
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-01 19:02:26 +0300

    feat: Add mock data and onDelete function

---
+++ src/components/Student/Student.jsx
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import { data } from "../mock/data";
+
+export class Student extends  Component{
+  constructor() {
+    super()
+    this.state = {
+      data: data
+    }
+  }
+  render() {
+
+    const onDelete = (id) => {
+      let result = this.state.data.filter((item) => item.id !== id)
+      console.log(result);
+      this.setState({data: result})
+    }
+
+    return (
+      <div className="container">
+        <div className="wrapper">
+        <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Address</th>
+            <th>Status</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.data.map(({id, name, age, status, address}) => {
+              return(
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>{age}</td>
+                  <td>{address}</td>
+                  <td>{status}</td>
+                  <td>
+                    <button onClick={() => onDelete(id)}>
+                      Delete
+                    </button>
+                    <button>
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+        </div>
+      </div>
+    )
+  }
+}
