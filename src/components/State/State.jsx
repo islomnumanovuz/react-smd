a93d51205d354714f3f2eb403286b023caefe46d
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-01 23:11:08 +0300

    feat: Add basic example of styled component

--- src/components/State/State.jsx
+++ src/components/State/State.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Container, Title} from "./StateStyle";
 
 export class State extends Component {
   constructor(props) {
@@ -19,6 +20,10 @@ export class State extends Component {
           <h2>Surname {this.state.surname}</h2>
           <input name="name" onChange={onChange} type="text" placeholder="name" />
           <input name="surname" onChange={onChange} type="text" placeholder="surname" />
+          <hr/>
+          <Container className="title">
+            <Title className="subtitle">Styled component</Title>
+          </Container>
         </form>
       </div>
     )
---
+++ src/components/State/StateStyle.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled("div")`
+  width: 80%;
+  margin: 0 auto;
+  background-color: green;
+  padding: 20px;
+`
+const Title = styled.h2`
+  text-align: center;
+  color: white;
+`
+
+export {Container, Title}
