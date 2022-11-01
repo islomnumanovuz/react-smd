5a199c10c396d17493fcb68a98d2157b36194274
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-01 19:43:22 +0300

    feat: Add inputs and delete function

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -19,23 +19,22 @@ export class Student extends  Component{
     return (
       <div className="container">
         <div className="wrapper">
-        <div className="inputs">
-					<input
-						placeholder="Search by Id"
-						name="id"
-					></input>
-					<input
-						placeholder="Search by Name"
-						name="name"
-					></input>
-					<input
-						placeholder="Search by Status"
-						name="status"
-					></input>
-				</div>
-        
         <table>
         <thead>
+          <tr>
+            <th colSpan={6}>
+            <h6 className="text-center">Search item</h6>
+        <div className="input-group input-group-sm">
+        <input type="text" className="form-control" placeholder="Id" 
+          aria-label="Id" />
+          <input type="text" className="form-control" placeholder="Name" aria-label="Name" />
+          <input type="text" className="form-control" placeholder="Status" 
+          aria-label="Status" />
+          <span className="input-group-text">{this.state.data.length}</span>
+
+        </div>
+            </th>
+          </tr>
           <tr>
             <th>ID</th>
             <th>Name</th>
