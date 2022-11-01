39929e41701d9df4f80e6f84b136495a5cf29761
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-01 20:55:16 +0300

    feat: Add delete and add function

--- src/components/Student/Student.jsx
+++ src/components/Student/Student.jsx
@@ -5,14 +5,39 @@ export class Student extends  Component{
   constructor() {
     super()
     this.state = {
-      data: data
+      data: data,
+      name: "",
+      age: "",
+      address: "",
+      status: "",
+
     }
   }
   render() {
+    const onChange = event => {
+      this.setState({[event.target.name]: event.target.value})
+
+    }
 
     const onDelete = (id) => {
       let result = this.state.data.filter((item) => item.id !== id)
       this.setState({data: result})
+    }
+    const onAdd = () => {
+      let newUser = {
+        id: data.length + 1,
+        name: this.state.name[0].toUpperCase() + this.state.name.slice(1),
+        age: this.state.age,
+        address: this.state.address[0].toUpperCase() + this.state.address.slice(1),
+        status: this.state.status.toUpperCase(),
+      }
+      this.setState({
+        data: [...this.state.data, newUser],
+        name: "",
+        age: "",
+        address: "",
+        status: "",
+      })
     }
 
     return (
@@ -23,13 +48,16 @@ export class Student extends  Component{
           <tr>
             <th colSpan={6}>
               <h6 className="text-center">Search item</h6>
-              <div className="input-group input-group-sm">
-              <input type="text" className="form-control" placeholder="Id" 
-                aria-label="Id" />
-                <input type="text" className="form-control" placeholder="Name" aria-label="Name" />
-                <input type="text" className="form-control" placeholder="Status" 
+              <div className="input-group input-group-sm pb-1">
+                <input value={this.state.name} name="name" onChange={onChange} type="text" className="form-control" placeholder="Name" aria-label="Name" />
+                <input value={this.state.age} name="age" onChange={onChange} type="number" className="form-control" placeholder="Age" 
+                aria-label="Age" />
+                <input value={this.state.address} name="address" onChange={onChange} type="text" className="form-control" placeholder="Address" 
+                aria-label="Address" />
+                <input value={this.state.status} name="status" onChange={onChange} type="text" className="form-control" placeholder="Status" 
                 aria-label="Status" />
-                <span className="input-group-text">{this.state.data.length}</span>
+                
+                <button className="btn btn-primary" onClick={onAdd}>Add item</button>
 
               </div>
             </th>
@@ -55,10 +83,10 @@ export class Student extends  Component{
                   <td>{address}</td>
                   <td>{status}</td>
                   <td>
-                    <button onClick={() => onDelete(id)}>
+                    <button className="btn btn-outline-danger" onClick={() => onDelete(id)}>
                       Delete
                     </button>
-                    <button>
+                    <button className="btn btn-outline-secondary">
                       Edit
                     </button>
                   </td>
