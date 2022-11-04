5afab95c1f5241430c0a77dc8785d4736ae7e3d2
islomnumanov <islomnumanov.uz@gmail.com>
2022-11-04 15:38:48 +0300

    feat: Add function in styled componenets with switch case

--- src/components/State/StateStyle.js
+++ src/components/State/StateStyle.js
@@ -10,10 +10,20 @@ const Title = styled.h2`
   text-align: center;
   color: white;
 `
+const getSize = (props) => {
+  switch (props.type) {
+    case "large": return "200px";
+    case "medium": return "150px";
+    case "small": return "100px";
+    default: return "150px"
+  }
+}
 const Box = styled("div")`
   width: ${(props) => props.type === "large" ? "200px" : "100px"};
   height: ${(props) => props.type === "large" ? "200px" : "100px"};
   background-color: blue;
   border-radius: 50%;
+  width: ${getSize};
+  height: ${getSize};
 `
 export {Container, Title, Box}
